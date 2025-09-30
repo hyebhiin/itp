a57b39d94ee40efd5c88b89510238a8c978d84ee
hyebhiin <nohyebin04@gmail.com>
2025-09-30 09:12:43 -0400

    Update F to C.js

--- FtoC.js/F to C.js
+++ FtoC.js/F to C.js
@@ -1,4 +1,4 @@
 // Javascript F to C
 // let f = 99;
 // let c = (f - 32) * 5 / 9;
-console.log(f + "°F is equal to " + c.toFixed(2) + "°C");
+console.log(f + "Ferenheit is equal to " + c.toFixed(2) + "celsius");
